5fbef04d7257e1d0cfec17448d783ea9e00ddafa
Ghada Saguer <ghadst@gmil.com>
2018-05-30 15:21:28 +0200

    jout du Routing de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import {HttpClientModule} from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { Routes, RouterLink,RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
+const _routes: Routes= [
+  // objets json de configuration de routage
+{path: 'posts', component: PostListComponent},
+{path: 'welcome', component: WelcomeComponent},
+{path: '', redirectTo:'/welcome',pathMatch:'full'},
+{path: '**', component: NotfoundComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotfoundComponent,
+    WelcomeComponent
   ],
-  imports: [
+  imports:
+   [RouterModule.forRoot(_routes),
     BrowserModule,
-   HttpClientModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
